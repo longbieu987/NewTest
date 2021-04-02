636fb8c6cfc86433506b4d644b809f4dc6a4c7e7
Long <nguyenhieulong1109@gmail.com>
2021-04-02 10:22:56 +0700

    ee
    ee

--- app/src/main/java/com/example/myapplication/TestThread.kt
+++ app/src/main/java/com/example/myapplication/TestThread.kt
@@ -6,32 +6,45 @@ import java.util.*
 fun main() {
     var list = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
     Thread {
-        for (i in 0 until list.size) {
-            list.set(i, i * i)
-            println("${Thread.currentThread().name} : list.get($i) = ${list.get(i)}")
-            Thread.sleep(10)
-        }
-        print("\n")
-    }.start()
-
-    Thread {
-        for (i in 0 until list.size) {
-            if (i % 2 == 0) {
-                list.set(i, 0)
+        try {
+            for (i in 0 until list.size) {
+                if(i%2==0)
+                list.remove(i)
+                println("${Thread.currentThread().name} list.get($i): ${list.get(i)}")
+                Thread.sleep(100)
             }
-            println("${Thread.currentThread().name} : list.get($i) = ${list.get(i)}")
-            Thread.sleep(10)
+            print("\n")
+        } finally {
+            println("List = $list")
         }
-        print("\n")
     }.start()
-
     Thread {
-        for (i in 0 until list.size) {
-            list.set(i, i + 5)
-            println("${Thread.currentThread().name} : list.get($i) = ${list.get(i)}")
-            Thread.sleep(10)
+        try {
+            for (i in 0 until list.size) {
+
+                if (i % 2 == 1) {
+                    list.add(i + 4)
+                }
+                println("${Thread.currentThread().name} list.get($i): ${list.get(i)}")
+                Thread.sleep(100)
+            }
+            print("\n")
+        } finally {
+            println("List = $list")
         }
-        print("\n")
+    }.start()
+    Thread {
+        try {
+            for (i in 0 until list.size) {
+
+                list.set(i, i + 5)
+                println("${Thread.currentThread().name} list.get($i): ${list.get(i)}")
+                Thread.sleep(100)
+            }
+            print("\n")
+        } finally {
+            println("List = $list")
+        }
     }.start()
 
 
