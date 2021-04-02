1a9637936b26d93ef1793c49cb257f70b1b3574b
Long <nguyenhieulong1109@gmail.com>
2021-04-02 12:58:27 +0700

    Update TestThread.kt

--- app/src/main/java/com/example/myapplication/TestThread.kt
+++ app/src/main/java/com/example/myapplication/TestThread.kt
@@ -1,49 +1,53 @@
 package com.example.myapplication
 
-import kotlinx.coroutines.Dispatchers
-import java.util.*
+import kotlinx.coroutines.runBlocking
 
-fun main() {
-    var list = arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-    Thread {
+fun main(){
+    var list = arrayListOf(0,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+    val thread1 =Thread {
         try {
-            for (i in 0 until list.size) {
-                list.remove(i)
-                println("${Thread.currentThread().name} list.get($i): ${list.get(i)}")
-                Thread.sleep(100)
-            }
-            print("\n")
+                for(i in list.size-1 downTo 0) {
+                    if (i % 2 == 0) {
+                        println("${Thread.currentThread().name} Xóa ptu thứ $i : ${list.get(i)}")
+                        list.remove(i)
+                    }
+                    Thread.sleep(50)
+                }
         } finally {
-            println("List = $list")
+            println("List sau chỉnh sửa = $list")
+            print("\n")
         }
-    }.start()
-    Thread {
+    }
+    val thread2 =Thread {
         try {
             for (i in 0 until list.size) {
                 if (i % 4 == 0) {
-                    list.add(i,i+100)
+                    println("${Thread.currentThread().name} Thêm ptu vào vtri $i: ${i+100}")
+                    list.add(i, i + 100)
                 }
-                println("${Thread.currentThread().name} list.get($i): ${list.get(i)}")
-                Thread.sleep(100)
+                Thread.sleep(50)
             }
-            print("\n")
         } finally {
-            println("List = $list")
+            println("List sau chỉnh sửa = $list")
+            print("\n")
         }
-    }.start()
-    Thread {
+    }
+    val thread3 = Thread {
         try {
             for (i in 0 until list.size) {
-
+                println("${Thread.currentThread().name} Sửa ptu thứ $i: ${list.get(i)} = ${i+5}")
                 list.set(i, i + 5)
-                println("${Thread.currentThread().name} list.get($i): ${list.get(i)}")
-                Thread.sleep(100)
+                Thread.sleep(50)
             }
-            print("\n")
         } finally {
-            println("List = $list")
+            println("List sau chỉnh sửa = $list")
+            print("\n")
         }
-    }.start()
-
-
+    }
+    thread1.start()
+    thread1.join()
+    thread2.start()
+    thread2.join()
+    thread3.start()
+    thread3.join()
 }
